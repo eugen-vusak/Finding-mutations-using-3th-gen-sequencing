87b9465e052d942121bbb685c5e3f57505fb73c2
Eugen Vusak <eugen.vusak@gmail.com>
2020-01-05 19:07:26 +0100

    add comments for FastaFile class

--- src/FASTA/FastaFile.cpp
+++ src/FASTA/FastaFile.cpp
@@ -15,6 +15,7 @@ FastaFile::~FastaFile() {
 }
 
 bool FastaFile::open() {
+    // open input file stream
     file_stream_ = std::ifstream(filename_);
 
     if(!file_stream_.good()) {
@@ -22,7 +23,7 @@ bool FastaFile::open() {
         return false;
     }
 
-
+    // read first header to see if any records exist
     readHeader();
 
     return true;
@@ -34,10 +35,14 @@ void FastaFile::close() {
         return;
     }
 
+    // close stream and clear has_next_record flag
+    has_next_record_ = false;
     file_stream_.close();
 }
 
 void FastaFile::readHeader() {
+    // read and ignore all lines until header line
+    // when it's reached store it and mark existance of next record
 
     std::string line;
     while( std::getline( file_stream_, line ).good()) {
@@ -67,22 +72,27 @@ FastaRecord FastaFile::getNextRecord() {
     }
 
     if (! has_next_record_) {
-        throw std::out_of_range("No more records");
+        throw std::out_of_range("No more records. See hasNextRecord()");
     }
 
     FastaRecord record;
+
+    // set cached header line and clear has_next_record flag
     record.setHeader(next_header_);
     has_next_record_ = false;
 
     std::string line;
     while( std::getline( file_stream_, line ).good()) {
 
+        // if line is header (starts with '>')
+        // save header line and mark existance of next record
         if (line[0] == '>') {
             next_header_ = line;
             has_next_record_ = true;
             break;
         }
 
+        // else line is sequence line of record thus extend current sequence with it
         record.extendSequence(line);
     }
 
