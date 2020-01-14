d2d33177f9806988c780df88eecd0067c6c001d8
Eugen Vusak <eugen.vusak@gmail.com>
2020-01-14 14:53:05 +0100

    in alignment add partialAlign change readAlign to completeAlign, add comment for ETA, fix implicit conversions

--- include/align/alignment.hpp
+++ include/align/alignment.hpp
@@ -1,12 +1,35 @@
 #include "FASTA/FastaRecord.hpp"
-#include "FASTA/FastaFile.hpp"
 #include "align/SmithWaterman.hpp"
 #include "map/mapping.hpp"
 
-
 namespace alignment {
 
-void alignRead(FastaRecord& reference, FastaRecord& read, mapping::Band band,
-               SmithWaterman::MutationsTupleVector& mutations);
+void completeAlign(const FastaRecord& read,
+                   const FastaRecord& reference,
+                   const mapping::Band& band,
+                   SmithWaterman::MutationsTupleVector& mutations);
+
+/**
+ * @brief Aligns read to reference but uses partial read
+ *
+ * For alignment only part of read that is after first minimizer
+ * and before last minimizer is used.
+ *
+ * Example:
+ * read:    ____FFFF______LLL_____
+ * used:    --------+++++++++-----
+ *
+ * - where F marks first minimizer and L marks last minimizer
+ *   and where - marks unused portion of read and + marks used portion
+ *
+ * @param read that will be aligned
+ * @param reference to which read will be aligned
+ * @param band mapping band that tells where read maps to reference
+ * @param mutations rezulting mutations from alignment process
+ */
+void partialAlign(const FastaRecord& read,
+                  const FastaRecord& reference,
+                  const mapping::Band& band,
+                  SmithWaterman::MutationsTupleVector& mutations);
 
 }
--- src/align/alignment.cpp
+++ src/align/alignment.cpp
@@ -1,29 +1,40 @@
 #include "align/alignment.hpp"
 
-
 #include <iostream>
 #include <algorithm>
-#include <set>
 
+/**
+ * @brief  ETA parameter marks how long of extra segment on
+ * each side of reference will be used for alignment
+ *
+ * Example:
+ *  - read:            XXXXXX
+ *  - reference:    EEEYYYYYYEEE
+ * where E letter marks extra position due to ETA=3
+ */
 #define ETA 10
 
-
-static bool compareByPos(const mapping::Pair &lhs, const mapping::Pair &rhs) {
+static bool compareByPos(const mapping::Pair &lhs,
+                         const mapping::Pair &rhs) {
     return lhs.second < rhs.second;
 }
 
-void alignment::alignRead(FastaRecord& reference, FastaRecord& read, mapping::Band band,
-                          SmithWaterman::MutationsTupleSet& mutations) {
+void alignment::completeAlign(const FastaRecord& read,
+                              const FastaRecord& reference,
+                              const mapping::Band& band,
+                              SmithWaterman::MutationsTupleVector& mutations) {
 
     if (band.empty()) {
         return;
     }
 
-    std::vector<mapping::Pair>::iterator first_pair = std::min_element(band.begin(), band.end(), compareByPos);
-    Seed first_seed = first_pair->second;
+    auto first_pair = std::min_element(band.begin(), band.end(), compareByPos);
+    const Seed& first_seed = first_pair->second;
 
     uint32_t start_read = first_seed.getStartReadPostion();
-    int32_t start_ref = first_seed.getStartReferencePostion() - start_read - ETA;
+    int32_t start_ref = static_cast<int32_t>(
+                            first_seed.getStartReferencePostion() -
+                            start_read - ETA);
 
     int32_t ref_out_of_range = 0;
     if(start_ref < 0) {
@@ -31,13 +42,47 @@ void alignment::alignRead(FastaRecord& reference, FastaRecord& read, mapping::Ba
         start_ref = 0;
     }
 
-    uint32_t seq_len = read.getSequence().size() + 2 * ETA + ref_out_of_range;
-    std::string ref_seq_segment = reference.getSequence().substr(start_ref, seq_len);
+    uint32_t seq_len = static_cast<uint32_t>(
+                           static_cast<int32_t>(read.getSequence().size()) +
+                           2 * ETA + ref_out_of_range);
 
-    std::cout << "->" << std::endl;
+    std::string ref_seq_segment = reference.getSequence().substr(
+                                      static_cast<unsigned>(start_ref),
+                                      seq_len);
 
-    bool blosum_use = false;
-    SmithWaterman sw(read.getSequence(), ref_seq_segment, blosum_use);
+    SmithWaterman sw(read.getSequence(), ref_seq_segment, false);
 
-    sw.reconstruct_path(start_ref, mutations);
+    sw.reconstruct_path(static_cast<unsigned>(start_ref), mutations);
+}
+
+void alignment::partialAlign(const FastaRecord& read,
+                             const FastaRecord& reference,
+                             const mapping::Band& band,
+                             SmithWaterman::MutationsTupleVector& mutations) {
+
+    // ignore empty bands
+    if (band.empty()) {
+        return;
+    }
+
+    // find first and last element in band
+    auto minmax_el = std::minmax_element(band.begin(), band.end(), compareByPos);
+    const Seed& first_seed = minmax_el.first->second;
+    const Seed& last_seed = minmax_el.second->second;
+
+    // included area on read
+    uint32_t read_start = first_seed.getStartReadPostion() + first_seed.getSizeRead();
+    uint32_t read_end = last_seed.getStartReadPostion();
+
+    // included area on reference
+    uint32_t ref_start = first_seed.getStartReferencePostion() + first_seed.getSizeReference();
+    uint32_t ref_end = last_seed.getStartReferencePostion();
+
+    SmithWaterman sw(
+        read.getSequence().substr(read_start, read_end - read_start),
+        reference.getSequence().substr(ref_start, ref_end - ref_start),
+        false       // don't use blosum table
+    );
+
+    sw.reconstruct_path(ref_start, mutations);
 }
